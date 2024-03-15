0da09cb221276f8ee3118af961fce197268722be
victorsedlak1 <victorsedlak04@gmail.com>
2024-03-15 15:02:54 -0300

    construindo um sistema

--- app/urls.py
+++ app/urls.py
@@ -9,11 +9,12 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet, CategoriaViewSet
+from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet
 
 router = DefaultRouter()
 
 router.register(r"categorias", CategoriaViewSet, basename="categorias")
+router.register(r"editoras", CategoriaViewSet, basename="editoras")
 router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
